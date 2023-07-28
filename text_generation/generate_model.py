ce65dcc8e641f5232d226b4bf6a8c2b3b8505574
xarxaxdev <xarxax.dev@gmail.com>
2023-07-28 04:28:45 +0200

    added base code for all tutorials

--- text_generation/generate_model.py
+++ text_generation/generate_model.py
@@ -1,43 +1,77 @@
 from datasets import load_dataset
 
-imdb = load_dataset("imdb")
+squad = load_dataset("squad", split="train[:5000]")
 
-
+squad = squad.train_test_split(test_size=0.2)
 from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
 
+
 def preprocess_function(examples):
-    return tokenizer(examples["text"], truncation=True)
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=384,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        answer = answers[i]
+        start_char = answer["answer_start"][0]
+        end_char = answer["answer_start"][0] + len(answer["text"][0])
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label it (0, 0)
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
 
 
-tokenized_imdb = imdb.map(preprocess_function, batched=True)
-
-from transformers import DataCollatorWithPadding
-
-data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-import evaluate
-
-accuracy = evaluate.load("accuracy")
-
-import numpy as np
+tokenized_squad = squad.map(preprocess_function, batched=True, remove_columns=squad["train"].column_names)
 
 
-def compute_metrics(eval_pred):
-    predictions, labels = eval_pred
-    predictions = np.argmax(predictions, axis=1)
-    return accuracy.compute(predictions=predictions, references=labels)
+from transformers import DefaultDataCollator
+
+data_collator = DefaultDataCollator()
 
 
-id2label = {0: "NEGATIVE", 1: "POSITIVE"}
-label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+from transformers import AutoModelForQuestionAnswering, TrainingArguments, Trainer
 
+model = AutoModelForQuestionAnswering.from_pretrained("distilbert-base-uncased")
 
-from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
-
-model = AutoModelForSequenceClassification.from_pretrained(
-    "distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id
-)
 
 epochs = 1 
 lr = 2e-5 
