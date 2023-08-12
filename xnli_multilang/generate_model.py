d5db2de01bd72de3e3a66eb489c3c1a015865ca8
xarxaxdev <xarxax.dev@gmail.com>
2023-08-12 17:20:18 +0200

    cuda testing

--- xnli_multilang/generate_model.py
+++ xnli_multilang/generate_model.py
@@ -1,54 +1,29 @@
+#DATASET LOADING
 from datasets import load_dataset
-
-
 languages = ['ar', 'bg', 'de', 'el', 'en', 'es', 'fr', 'hi', 'ru', 'sw', 'th', 'tr', 'ur', 'vi', 'zh']
+languages = ['es']
 #languages.append['all_languages'] #I'll think what to do with this one
-
 xnli = {}
 for l in languages:
     xnli[l] = load_dataset("xnli",l)
-#xnli = load_dataset("xnli",'ar')
 
+
+#TOKENIZING
 base_model = "bert-base-multilingual-cased"
 from transformers import AutoTokenizer
-
 tokenizer = AutoTokenizer.from_pretrained(base_model)
-
 def preprocess_function(examples):
-    print(examples)
-    return tokenizer(examples["premise"] +tokenizer.sep_token + examples["premise"], truncation=True)
-
-def preprocess_function_p(examples):
-    return tokenizer(examples["premise"], truncation=True)
-
-def preprocess_function_h(examples):
-    return tokenizer(examples["hypothesis"],truncation=True)
-
-#tokenized_xnli_p = {}
-#tokenized_xnli_h = {}
+    return tokenizer(examples["premise"], examples["hypothesis"], truncation=True)
 tokenized_xnli = {}
 for l in languages:
-    #tokenized_xnli_p[l] = {}
-    #tokenized_xnli_h[l] = {}
-    #for s in ['train','test']:
-    #    tokenized_xnli_p[l][s] = xnli[l][s].map(preprocess_function_p, batched=True)
-    #    tokenized_xnli_h[l][s] = xnli[l][s].map(preprocess_function_h, batched=True)
     tokenized_xnli[l] = xnli[l].map(preprocess_function, batched=True)
-    
-#print(tokenized_xnli_p['es']['train'][0])
-#print(tokenized_xnli_h['es']['train'][0])
-print(tokenized_xnli['es']['train'][0])
-assert(False)
+#COLLATION    
 from transformers import DataCollatorWithPadding
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+#EVALUATION
 import evaluate
-
 f1 = evaluate.load("f1")
-
 import numpy as np
-
-
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions, axis=1)
@@ -60,17 +35,18 @@ label2id = {"NEGATIVE": 0, "POSITIVE": 1}
 
 
 from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+#assert(False)
 
+epochs = 1 
+lr = 2e-5 
 
 for l in languages:
-
+    print(f'training model for {l}')
     model = AutoModelForSequenceClassification.from_pretrained(
         base_model, num_labels=2, id2label=id2label, label2id=label2id
-    ).to('cuda')
+    )#.to('cuda')
+    model_name = f'{base_model}_lr{lr}_epochs{epochs}_l={l}'
 
-    epochs = 1 
-    lr = 2e-5 
-    model_name = f'{base_model}_lr{lr}_epochs{epochs}_l='
     training_args = TrainingArguments(
         output_dir=f"~/generated_models/{model_name}",
         learning_rate=2e-5,
@@ -87,8 +63,8 @@ for l in languages:
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=tokenized_xnli["train"],
-        eval_dataset=tokenized_xnli["test"],
+        train_dataset=tokenized_xnli[l]["train"],
+        eval_dataset=tokenized_xnli[l]["test"],
         tokenizer=tokenizer,
         data_collator=data_collator,
         compute_metrics=compute_metrics,
